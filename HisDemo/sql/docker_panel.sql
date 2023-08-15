4e99a06f593bd5030ef75d34b16541dd5ceedf02
gfkgfk <gfk000@126.com>
2023-08-15 22:43:22 +0800

    Update docker_panel.sql

--- HisDemo/sql/docker_panel.sql
+++ HisDemo/sql/docker_panel.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50650
  File Encoding         : 65001
 
- Date: 15/08/2023 22:41:36
+ Date: 15/08/2023 22:43:06
 */
 
 SET NAMES utf8mb4;
@@ -26,16 +26,17 @@ CREATE TABLE `console_announcement`  (
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公告标题',
   `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '公告内容',
   `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `is_deleted` tinyint(255) NOT NULL COMMENT '是否已删除',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of console_announcement
 -- ----------------------------
-INSERT INTO `console_announcement` VALUES (1, '关于省健康信息平台数据采集服务项目', '省全民健康信息平台共享数据集与数据标准（中医药部分）的通知、关于开展全省二级公立中医医院数据采集工作的通知要求，省卫生健康委、省中医药管理局开展了省医疗服务综合监管系统项目建设，实现对全省二级中医医院数据的采集、转换、传输、控制、分析和监管，同时省中医药管理局部署了统一的数据采集工具，各医院需要通过该工具来实现与省全民健康信息平台的对接。', '2023-08-03 09:53:36');
-INSERT INTO `console_announcement` VALUES (2, '大数据医疗智慧健康管理系统平台', '大数据医疗智慧健康管理系统平台通过智能硬件、边缘计算采集数据,平台做健康评估,数据分析,状态预测,异常预报预警,决策维护.', '2023-08-03 10:23:37');
-INSERT INTO `console_announcement` VALUES (3, '医疗数据采集', '实时识别,记录医患问诊内容.向医生进行问诊内容提示,患教知识推荐,用药安全提醒等.可对接电子病历系统上传病历,提升医疗质量,提高临床效率,加速医院的数字化进程.', '2023-08-03 10:24:30');
-INSERT INTO `console_announcement` VALUES (4, '关于定点医疗机构上传数据质量', '准确采集医疗服务数据信息 在采集参保人员医疗服务相关信息时,应按照要求正确分项录入医疗费用及相关信息,特别是准确采集入', '2023-08-03 10:25:08');
+INSERT INTO `console_announcement` VALUES (1, '关于省健康信息平台数据采集服务项目', '省全民健康信息平台共享数据集与数据标准（中医药部分）的通知、关于开展全省二级公立中医医院数据采集工作的通知要求，省卫生健康委、省中医药管理局开展了省医疗服务综合监管系统项目建设，实现对全省二级中医医院数据的采集、转换、传输、控制、分析和监管，同时省中医药管理局部署了统一的数据采集工具，各医院需要通过该工具来实现与省全民健康信息平台的对接。', '2023-08-03 09:53:36', 0);
+INSERT INTO `console_announcement` VALUES (2, '大数据医疗智慧健康管理系统平台', '大数据医疗智慧健康管理系统平台通过智能硬件、边缘计算采集数据,平台做健康评估,数据分析,状态预测,异常预报预警,决策维护.', '2023-08-03 10:23:37', 0);
+INSERT INTO `console_announcement` VALUES (3, '医疗数据采集', '实时识别,记录医患问诊内容.向医生进行问诊内容提示,患教知识推荐,用药安全提醒等.可对接电子病历系统上传病历,提升医疗质量,提高临床效率,加速医院的数字化进程.', '2023-08-03 10:24:30', 0);
+INSERT INTO `console_announcement` VALUES (4, '关于定点医疗机构上传数据质量', '准确采集医疗服务数据信息 在采集参保人员医疗服务相关信息时,应按照要求正确分项录入医疗费用及相关信息,特别是准确采集入', '2023-08-03 10:25:08', 0);
 
 -- ----------------------------
 -- Table structure for console_feedback
