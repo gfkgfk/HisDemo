10da4d611b6271d7498a6d728584bebcf5afba35
gfkgfk <gfk000@126.com>
2023-10-21 12:19:54 +0800

    Update docker_panel.sql

--- HisDemo/sql/docker_panel.sql
+++ HisDemo/sql/docker_panel.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 93.179.125.238_3306
+ Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 50650
- Source Host           : 93.179.125.238:3306
+ Source Server Version : 80100
+ Source Host           : localhost:3306
  Source Schema         : docker_panel
 
  Target Server Type    : MySQL
- Target Server Version : 50650
+ Target Server Version : 80100
  File Encoding         : 65001
 
- Date: 15/08/2023 22:43:06
+ Date: 21/10/2023 12:19:11
 */
 
 SET NAMES utf8mb4;
@@ -22,13 +22,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `console_announcement`;
 CREATE TABLE `console_announcement`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键ID',
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '公告标题',
-  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '公告内容',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `is_deleted` tinyint(255) NOT NULL COMMENT '是否已删除',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
+                                         `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增主键ID',
+                                         `title` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '公告标题',
+                                         `content` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '公告内容',
+                                         `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+                                         `is_deleted` tinyint NOT NULL COMMENT '是否已删除',
+                                         PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of console_announcement
@@ -43,34 +43,35 @@ INSERT INTO `console_announcement` VALUES (4, '关于定点医疗机构上传数
 -- ----------------------------
 DROP TABLE IF EXISTS `console_feedback`;
 CREATE TABLE `console_feedback`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '反馈内容',
-  `user_id` bigint(255) NOT NULL COMMENT '反馈填写医生ID',
-  `is_deleted` tinyint(255) NOT NULL COMMENT '是否删除',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+                                     `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                     `content` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '反馈内容',
+                                     `user_id` bigint NOT NULL COMMENT '反馈填写医生ID',
+                                     `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+                                     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of console_feedback
 -- ----------------------------
-INSERT INTO `console_feedback` VALUES (1, '测试', 1, 0);
+INSERT INTO `console_feedback` VALUES (1, '高血压', 1, 0);
 INSERT INTO `console_feedback` VALUES (2, '测试2', 1, 0);
 INSERT INTO `console_feedback` VALUES (3, '测试3', 1, 0);
+INSERT INTO `console_feedback` VALUES (4, '321321', 1, 0);
 
 -- ----------------------------
 -- Table structure for console_measure
 -- ----------------------------
 DROP TABLE IF EXISTS `console_measure`;
 CREATE TABLE `console_measure`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '测量设备类型',
-  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '测量值',
-  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
-  `user_id` bigint(20) NOT NULL COMMENT '被测量人用户id',
-  `feedback_id` bigint(20) NOT NULL COMMENT '反馈信息ID',
-  `is_deleted` tinyint(255) NOT NULL COMMENT '是否已删除',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
+                                    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+                                    `type` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '测量设备类型',
+                                    `value` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '测量值',
+                                    `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+                                    `user_id` bigint NOT NULL COMMENT '被测量人用户id',
+                                    `feedback_id` bigint NOT NULL COMMENT '反馈信息ID',
+                                    `is_deleted` tinyint NOT NULL DEFAULT 0 COMMENT '是否已删除',
+                                    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of console_measure
@@ -78,19 +79,22 @@ CREATE TABLE `console_measure`  (
 INSERT INTO `console_measure` VALUES (1, '1', '100/86', '2023-08-03 09:14:25', 1, 1, 0);
 INSERT INTO `console_measure` VALUES (2, '1', '91/89', '2023-08-03 10:27:57', 1, 2, 0);
 INSERT INTO `console_measure` VALUES (3, '1', '97/86', '2023-08-03 10:28:22', 1, 0, 0);
+INSERT INTO `console_measure` VALUES (4, '1', '99/85', '2023-10-21 11:35:06', 1, -1, 0);
+INSERT INTO `console_measure` VALUES (5, '1', '123/66', '2023-10-21 11:37:20', 1, -1, 0);
+INSERT INTO `console_measure` VALUES (6, '1', '123/87', '2023-10-21 12:16:58', 1, 4, 0);
 
 -- ----------------------------
 -- Table structure for console_menu
 -- ----------------------------
 DROP TABLE IF EXISTS `console_menu`;
 CREATE TABLE `console_menu`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `c_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `parent_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+                                 `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                 `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                 `c_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                 `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                 `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                 `parent_id` bigint NOT NULL,
+                                 PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -102,10 +106,10 @@ CREATE TABLE `console_menu`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `console_role`;
 CREATE TABLE `console_role`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `c_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+                                 `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                 `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                 `c_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                 PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -118,16 +122,16 @@ INSERT INTO `console_role` VALUES (1, 'admin', '管理员');
 -- ----------------------------
 DROP TABLE IF EXISTS `console_user`;
 CREATE TABLE `console_user`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
-  `phone_num` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
-  `gender` int(11) NULL DEFAULT NULL COMMENT '性别',
-  `role` int(11) NULL DEFAULT NULL COMMENT '角色',
-  `locked_flag` tinyint(4) NULL DEFAULT 0 COMMENT '是否被锁定:0-未被锁定;1-已被锁定',
-  `is_deleted` tinyint(4) NULL DEFAULT 0 COMMENT '是否被删除:0-未被删除;1-已被删除',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '注册日期',
-  PRIMARY KEY (`id`) USING BTREE
+                                 `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                 `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
+                                 `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+                                 `phone_num` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+                                 `gender` int NULL DEFAULT NULL COMMENT '性别',
+                                 `role` int NULL DEFAULT NULL COMMENT '角色',
+                                 `locked_flag` tinyint NULL DEFAULT 0 COMMENT '是否被锁定:0-未被锁定;1-已被锁定',
+                                 `is_deleted` tinyint NULL DEFAULT 0 COMMENT '是否被删除:0-未被删除;1-已被删除',
+                                 `create_time` datetime(0) NULL DEFAULT NULL COMMENT '注册日期',
+                                 PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -140,16 +144,16 @@ INSERT INTO `console_user` VALUES (1, 'kent', 'e10adc3949ba59abbe56e057f20f883e'
 -- ----------------------------
 DROP TABLE IF EXISTS `console_user_token`;
 CREATE TABLE `console_user_token`  (
-  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `update_time` datetime(0) NULL DEFAULT NULL,
-  `expire_time` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`user_id`) USING BTREE
+                                       `user_id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                       `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                                       `update_time` datetime(0) NULL DEFAULT NULL,
+                                       `expire_time` datetime(0) NULL DEFAULT NULL,
+                                       PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of console_user_token
 -- ----------------------------
-INSERT INTO `console_user_token` VALUES (1, '3edf4d3711685495598d285956c75f9-', '2023-08-15 22:33:34', '2023-08-17 22:33:34');
+INSERT INTO `console_user_token` VALUES (1, '28615c09c6cfb61528f0ac2063561deb', '2023-10-21 12:18:44', '2023-10-23 12:18:44');
 
 SET FOREIGN_KEY_CHECKS = 1;
